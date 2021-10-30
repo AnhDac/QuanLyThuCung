83138bbd00e5c8007d5c2445e395970c45230033
AnhDac <86392035+AnhDac@users.noreply.github.com>
2021-10-30 09:48:04 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using QLThuCung.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLThuCung
+{
+    public partial class Form1 : Form
+    {
+        ThụCungEntities db = new ThụCungEntities();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            var result = from c in db.Customers select new { IDCus = c.ID_Cus, Name = c.Name, Phone = c.Phone};
+            dgvTest.DataSource = result.ToList();
+        }
+    }
+}
