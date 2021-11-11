421888e493995c7abf97467ad3f3ba6dcbe91c40
AnhDac <86392035+AnhDac@users.noreply.github.com>
2021-11-11 19:53:45 +0700

    tao branch KyHome

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace QLThuCung
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new DangNhap());
+
         }
     }
 }
