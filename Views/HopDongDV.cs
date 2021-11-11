e16472ac5c4c5b31da266a395a647f48c23bf6a1
AnhDac <86392035+AnhDac@users.noreply.github.com>
2021-11-11 23:44:46 +0700

    nothing

---
+++ Views/HopDongDV.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLThuCung.Model;
+namespace QLThuCung.Views
+{
+    public partial class HopDongDV : Form
+    {
+        ThuCungEntities db = new ThuCungEntities();
+        public HopDongDV()
+        {
+            InitializeComponent();
+        }
+
+        private void HopDongDV_Load(object sender, EventArgs e)
+        {
+            LoadDVChamSoc();
+            LoadHopDongDV();
+ 
+        }
+
+        void LoadHopDongDV()
+        {
+            var result = from kq in db.Contract_Ser select new { IDConser = kq.ID_ConSer, IDEmp = kq.ID_Emp, IDCus = kq.ID_Cus, DateBuy = kq.DateBuy, Price = kq.Price };
+            var empl = db.Employees.ToList();
+            cbDV.DataSource = empl;
+            cbDV.DisplayMember = "ID_Emp";
+            cbDV.ValueMember = "ID_Emp";
+            cbHD.DataSource = result.ToList();
+            cbHD.DisplayMember = "ID_ConSer";
+            cbHD.ValueMember = "IDConser";
+            dgvHD.DataSource = result.ToList();
+            txtHD_SoLuong.Text = dgvHD.Rows.Count.ToString();
+        }
+        void binHopDongDV()
+        {
+            txtHD_IDConser.Text = dgvHD.CurrentRow.Cells[0].Value.ToString().Trim();
+            txtHD_IDEmp.Text = dgvHD.CurrentRow.Cells[1].Value.ToString().Trim();
+            txtHD_IDCus.Text = dgvHD.CurrentRow.Cells[2].Value.ToString().Trim();
+            txtHD_DateBuy.Text = dgvHD.CurrentRow.Cells[3].Value.ToString().Trim();
+            txtHD_Price.Text = dgvHD.CurrentRow.Cells[4].Value.ToString().Trim();
+        }
+
+        private void dgvHD_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            binHopDongDV();
+        }
+        
+        private void btnLuuHD_Click(object sender, EventArgs e)
+        { 
+               try
+                {
+                if (MessageBox.Show("Do you want update this Ser?", "update Ser", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    string id = dgvHD.SelectedCells[0].OwningRow.Cells[0].Value.ToString().Trim();
+                    Contract_Ser contract = db.Contract_Ser.Find(id);
+                    contract.ID_Emp = txtHD_IDEmp.Text.ToString().Trim();
+                    contract.ID_Cus = txtHD_IDCus.Text.ToString().Trim();
+                    contract.DateBuy = Convert.ToDateTime(txtHD_DateBuy.Text);
+                    contract.Price = Convert.ToInt32(txtHD_Price.Text);
+                    db.SaveChanges();
+
+                    MessageBox.Show("update Thành Công!", "Thong Bao", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadHopDongDV();
+                }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Lỗi không update được!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    db = new ThuCungEntities();
+                    LoadHopDongDV();
+                }
+            
+        }
+
+        private void btnHD_Reload_Click(object sender, EventArgs e)
+        {
+            LoadHopDongDV();
+        }
+
+        private void bthHD_Insert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Do you want insert this pet?", "Delete Pet", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    Contract_Ser contract = new Contract_Ser();
+
+                    contract.ID_ConSer = txtHD_IDConser.Text.ToString().Trim();
+                    contract.ID_Emp = txtHD_IDEmp.Text.ToString().Trim();
+                    contract.ID_Cus = txtHD_IDCus.Text.ToString().Trim();
+                    contract.DateBuy = Convert.ToDateTime(txtHD_DateBuy.Text);
+                    contract.Price = Convert.ToInt32(txtHD_Price.Text);
+
+
+                    db.Contract_Ser.Add(contract);
+                    db.SaveChanges();
+
+                    MessageBox.Show("Thêm Thành Công!", "Thong Bao", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadHopDongDV();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi Không Thêm Được!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                db = new ThuCungEntities();
+                LoadHopDongDV();
+            }
+        }
+
+        private void btnHD_xoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Do you want delete this Ser?", "Delete Ser", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    Contract_Ser Contrac = db.Contract_Ser.Where(p => p.ID_ConSer == txtHD_IDConser.Text.ToString().Trim()).SingleOrDefault();
+                    db.Contract_Ser.Remove(Contrac);
+                    db.SaveChanges();
+                    MessageBox.Show("Delete Successfull!", "Thong Bao", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadHopDongDV();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi Không Delete Được!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                db = new ThuCungEntities();
+                LoadHopDongDV();
+            }
+        }
+
+        private void bthHD_TimKiem_Click(object sender, EventArgs e)
+        {
+            String tam = cbDV.Text.ToString().Trim();
+            var kq = db.view_Contract(tam);
+            dgvHD.DataSource = kq.ToList();
+            txtHD_SoLuong.Text = dgvHD.Rows.Count.ToString();
+        }
+
+        private void bthHD_chiTiet_Click(object sender, EventArgs e)
+        {
+            String tam = cbHD.Text.ToString().Trim();
+            var kq = db.view_DetaiContracser(tam);
+            dgvChiTietHD.DataSource = kq.ToList();
+        }
+
+        private void bthHD_Thoat_Click(object sender, EventArgs e)
+        {
+            DialogResult tl =
+                 MessageBox.Show("Bạn muốn Thoát ???", "Thông báo",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (tl == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        //---------form dịch vụ chăm sóc thú cưng
+        void LoadDVChamSoc()
+        {
+            var result = from kq in db.Servics select new { IDSer = kq.ID_Ser, name = kq.Name, price = kq.Price };
+            comboBox1.DataSource = result.ToList();
+            comboBox1.DisplayMember = "name";
+            comboBox1.SelectedItem = "name";
+            dvgDV.DataSource = result.ToList();
+        }
+        void binDVChamSoc()
+        {
+            txtDV_IDSer.Text = dvgDV.CurrentRow.Cells[0].Value.ToString().Trim();
+            txtDV_name.Text = dvgDV.CurrentRow.Cells[1].Value.ToString().Trim();
+            txtDV_price.Text = dvgDV.CurrentRow.Cells[2].Value.ToString().Trim();
+        }
+
+        private void dvgDV_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            binDVChamSoc();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            LoadDVChamSoc();
+        }
+
+        private void btnDV_timKiem_Click(object sender, EventArgs e)
+        {
+            String tam = comboBox1.Text.ToString().Trim();
+            var result = from kq in db.Servics where kq.Name == tam select new { IDSer = kq.ID_Ser, name = kq.Name, price = kq.Price };
+            dvgDV.DataSource = result.ToList();
+        }
+
+        private void btnDV_sua_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Do you want update this servic?", "update servic", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    string id = dvgDV.SelectedCells[0].OwningRow.Cells[0].Value.ToString().Trim();
+                    Servic servic = db.Servics.Find(id);
+                    servic.Name =txtDV_name.Text.ToString().Trim();
+                    servic.Price = Convert.ToInt32( txtDV_price.Text.ToString().Trim());
+                    db.SaveChanges();
+
+                    MessageBox.Show("update Thành Công!", "Thong Bao", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadDVChamSoc();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi không update được!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                db = new ThuCungEntities();
+                LoadDVChamSoc();
+            }
+
+        }
+
+        private void btnDV_Them_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Do you want insert this servic?", "insert servic", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    Servic servic = new Servic();
+
+                    servic.ID_Ser = txtDV_IDSer.Text.ToString().Trim();
+                    servic.Name = txtDV_name.Text.ToString().Trim();
+                    servic.Price = int.Parse(txtDV_price.Text.ToString());
+    
+
+
+                    db.Servics.Add(servic);
+                    db.SaveChanges();
+
+                    MessageBox.Show("Thêm Thành Công!", "Thong Bao", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadDVChamSoc();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi Không Thêm Được!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                db = new ThuCungEntities();
+                LoadDVChamSoc();
+            }
+        }
+
+        private void btnDV_xoa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Do you want delete this servic?", "Delete servic", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    Servic servic = db.Servics.Where(p => p.ID_Ser == txtDV_IDSer.Text.ToString().Trim()).SingleOrDefault();
+                    db.Servics.Remove(servic);
+                    db.SaveChanges();
+                    MessageBox.Show("Delete Successfull!", "Thong Bao", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadDVChamSoc();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi Không Delete Được!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                db = new ThuCungEntities();
+                LoadDVChamSoc();
+            }
+        }
+    }
+}
---
+++ Views/Login.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace QLThuCung.Views
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btThoat = new System.Windows.Forms.Button();
+            this.btDN = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMK = new System.Windows.Forms.TextBox();
+            this.txtTK = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Azure;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.btThoat);
+            this.panel1.Controls.Add(this.btDN);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.txtMK);
+            this.panel1.Controls.Add(this.txtTK);
+            this.panel1.Location = new System.Drawing.Point(9, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(287, 333);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btThoat
+            // 
+            this.btThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThoat.Location = new System.Drawing.Point(170, 253);
+            this.btThoat.Name = "btThoat";
+            this.btThoat.Size = new System.Drawing.Size(89, 30);
+            this.btThoat.TabIndex = 18;
+            this.btThoat.Text = "Thoát";
+            this.btThoat.UseVisualStyleBackColor = true;
+            this.btThoat.Click += new System.EventHandler(this.btThoat_Click);
+            // 
+            // btDN
+            // 
+            this.btDN.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDN.Location = new System.Drawing.Point(14, 263);
+            this.btDN.Name = "btDN";
+            this.btDN.Size = new System.Drawing.Size(89, 30);
+            this.btDN.TabIndex = 17;
+            this.btDN.Text = "Đăng Nhập";
+            this.btDN.UseVisualStyleBackColor = true;
+            this.btDN.Click += new System.EventHandler(this.btDN_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 170);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Mật khẩu :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 97);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Tên Đăng Nhập :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Aqua;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(2, 2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(278, 81);
+            this.panel2.TabIndex = 7;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(89, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.label1.Size = new System.Drawing.Size(94, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtMK
+            // 
+            this.txtMK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMK.Location = new System.Drawing.Point(14, 196);
+            this.txtMK.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMK.Multiline = true;
+            this.txtMK.Name = "txtMK";
+            this.txtMK.PasswordChar = '*';
+            this.txtMK.Size = new System.Drawing.Size(253, 32);
+            this.txtMK.TabIndex = 3;
+            this.txtMK.UseSystemPasswordChar = true;
+            this.txtMK.TextChanged += new System.EventHandler(this.txtMK_TextChanged);
+            // 
+            // txtTK
+            // 
+            this.txtTK.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTK.Location = new System.Drawing.Point(14, 124);
+            this.txtTK.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTK.Multiline = true;
+            this.txtTK.Name = "txtTK";
+            this.txtTK.Size = new System.Drawing.Size(253, 33);
+            this.txtTK.TabIndex = 2;
+            this.txtTK.TextChanged += new System.EventHandler(this.txtTK_TextChanged);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 362);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btThoat;
+        private System.Windows.Forms.Button btDN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMK;
+        private System.Windows.Forms.TextBox txtTK;
+    }
+}
